853e2cfc0c175a0260a1fa03b6b80479eccf0dd7
BucurEva87 <bucur.eva87@gmail.com>
2023-04-23 22:43:10 +0300

    Modify code to abide linter's rules

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-	def index
-		render json: { version: 1, author: 'Eva-Lavinia Bucur' }
-	end
+  def index
+    render json: { version: 1, author: 'Eva-Lavinia Bucur' }
+  end
 end
